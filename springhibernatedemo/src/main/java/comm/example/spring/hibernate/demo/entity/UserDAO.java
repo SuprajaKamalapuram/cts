e2204ae58df4d864ec012f8a3e4d54c0ed908568
KamalapuramSupraja <kamalapuramsupraja12@gmail.com>
2020-01-23 04:23:53 -0800

    spring-hibernate

---
+++ springhibernatedemo/src/main/java/comm/example/spring/hibernate/demo/entity/UserDAO.java
@@ -0,0 +1,8 @@
+package comm.example.spring.hibernate.demo.entity;
+
+import comm.example.spring.hibernate.demo.entity.UserDetail;
+
+public interface UserDAO {
+	public UserDetail createUser(UserDetail user);
+
+}
